6d8221b07b1bfdd546dee8214865ea8efd725776
Jacob Bass <jacob@jacobbass.net>
2016-12-01 10:05:07 +1100

    final v2.0. final final

--- js/index.js
+++ js/index.js
@@ -1,69 +1,3 @@
-function grow(e) {
-  var oldSize = 34;
-
-  if (e.style.fontSize !== "") {
-    oldSize = parseInt(e.style.fontSize.replace('px', ''));
-  }
-
-  e.style.fontSize = oldSize + "px";
-  var oldWidth = e.scrollWidth;
-  var oldHeight = e.scrollHeight;
-
-  e.style.fontSize = (oldSize + 1) + "px";
-  var newSize = oldSize + 1;
-  var newWidth = e.scrollWidth;
-  var newHeight = e.scrollHeight;
-
-  var heightExceeded = (newHeight - oldHeight) > 0;
-  var widthExceeded = (newWidth- oldWidth) > 0;
-
-  if (heightExceeded || widthExceeded) {
-    return shrink(e)
-  } else {
-    return grow(e)
-  }
-}
-
-function shrink(e) {
-  var oldSize = (e.style.fontSize !== "") ? parseInt(e.style.fontSize.replace('px', '')) : 34;
-  var oldWidth = e.scrollWidth;
-  var oldHeight = e.scrollHeight;
-
-  e.style.fontSize = (oldSize - 1) + "px";
-  var newSize = oldSize - 1;
-  var newWidth = e.scrollWidth;
-  var newHeight = e.scrollHeight;
-
-  var heightExceeded = (newHeight - oldHeight) < 0;
-  var widthExceeded = (newWidth- oldWidth) < 0;
-
-  console.log(heightExceeded, widthExceeded)
-
-  if (!heightExceeded || !widthExceeded) {
-    return shrink(e)
-  }
-}
-
-function growCodeBlock() {
-  var section = document.querySelector('section.present');
-  if (section.querySelector('section.present')) section = section.querySelector('section.present');
-  var scaler = section.querySelector('pre');
-  var code = section.querySelector('code');
-
-  if (!scaler || !code) return;
-
-  scaler.style.width = '100%';
-  code.style.minHeight = '100vh';
-  code.style.overflow = 'visible';
-
-  grow(code);
-
-  code.style.minHeight = '100%';
-  code.style.maxHeight = '90vh';
-  code.style.lineHeight = '1em';
-  code.style.overflow = 'hidden';
-}
-
 Reveal.initialize({
   center: false,
   controls: false,
@@ -128,7 +62,6 @@ function showMonoFooter (event) {
 
 Reveal.addEventListener('slidechanged', function (event) {
   showMonoFooter(event);
-  //growCodeBlock();
 });
 
 Reveal.addEventListener('ready', function (event) {
---
+++ t
@@ -0,0 +1,27 @@
+the setup
+  lerna init -i
+  create two packages
+  lerna bootstrap
+  show linkage
+
+lets add some code
+  requires
+  build tools
+  watcher
+  linting
+  testing
+
+the developer experience
+  storybook
+  styleguide
+  livedemo?
+
+adding new projects
+  add a new project
+  run bootstrap
+  add a story
+  add a test
+
+let's make a change
+  change button to allow theming similar to bootstrap
+  watch change filter through system (closing components, then servers)
